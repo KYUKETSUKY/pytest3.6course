41c2097b495f15285e219cd8a25d5e1a614fd175
kyukertsuki1110 <thenaturals@mail.ru>
2025-02-05 17:34:01 +0300

    написал тест

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_items(browser):
+def test_of_checking_the_presence_add_to_cart_button(browser):
     browser.get(link)
     time.sleep(30)
     add_to_card_button=browser.find_elements(By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]')
