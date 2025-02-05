33d6794087691192c33c5840718310a25b5b96a3
kyukertsuki1110 <thenaturals@mail.ru>
2025-02-05 17:28:50 +0300

    написал тест

--- test_items.py
+++ test_items.py
@@ -13,5 +13,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_items(browser):
     browser.get(link)
     time.sleep(30)
-    add_to_card_button=browser.find_elements(By.CSS_SELECTOR, '[class="btn "]')
+    add_to_card_button=browser.find_elements(By.CSS_SELECTOR, '[class="btn btn-lg btn-primary btn-add-to-basket"]')
     assert len(add_to_card_button) is not 0, 'Кнопка добавления товара в корзину отсутствует'
